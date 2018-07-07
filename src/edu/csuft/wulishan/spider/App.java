97eb060995449db3a54ae3bb60d656811d889516
Wulishan1022 <40029888+Wulishan1022@users.noreply.github.com>
2018-07-07 15:41:42 +0800

    myblog

---
+++ src/edu/csuft/wulishan/spider/App.java
@@ -0,0 +1,18 @@
+package edu.csuft.wulishan.spider;
+
+/**
+ * 
+ * @author Wulishan
+ *
+ */
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Spider spider = new Spider();
+		spider.runs();
+		spider.print();
+	}
+
+}
